501f01988d1a3a9c295d323e93f87013f0647ffb
manoj-kale <54758918+manoj-mk@users.noreply.github.com>
2020-06-14 08:22:15 +0530

    Updates

---
+++ precache-manifest.0a53deb9e79363131e409b4b8328a416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c92ce3dc53e55a98816621d4f823d0",
+    "url": "/cron_schedule/index.html"
+  },
+  {
+    "revision": "8144755f6683149672b5",
+    "url": "/cron_schedule/static/css/main.fe49eb09.chunk.css"
+  },
+  {
+    "revision": "c1e01277c6a4dd926866",
+    "url": "/cron_schedule/static/js/2.575a175f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cron_schedule/static/js/2.575a175f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8144755f6683149672b5",
+    "url": "/cron_schedule/static/js/main.ee014ca3.chunk.js"
+  },
+  {
+    "revision": "1c3ed52039e5bff57da1",
+    "url": "/cron_schedule/static/js/runtime-main.0d6146e7.js"
+  }
+]);
